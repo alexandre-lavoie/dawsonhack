100b192eaa075b6a3ee04dedf0680f89ecaa16be
Alexandre Lavoie <tatti6204@hotmail.com>
2018-05-06 12:06:39 -0400

    Added pictures and changed the read me.

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,31 +1,49 @@
+// Pin Setting
 const int triggerPin = 2;
-const int listenPin = 3;
+const int echoPin = 3;
 const int lMotorPin = 10;
+const int headPin = 11;
 const int rMotorPin = 12;
-const int TSpeed = 20;
-const int FLSpeed = 45;
-const int FRSpeed = 44;
-const int FDelay = 500;
-const int TDelay = 2080;
-const int Sleep = 10;
-const int MinDistance = 40;
-bool Driving = false;
+
+// Forward and Turn Speed Setting
+const int TLSpeed = 20;
+const int TRSpeed = 13;
+const int FLSpeed = 50;
+const int FRSpeed = 60;
+
+// Delays For Keeping the Pins High.
+const int ForwardDelay = 50;
+const int CoastTurnDelay = 8;
+const long TurnDelay = 1040;
+
+// Misc Variables.
+const int MinDistance = 35; // Min distance to stop when seeing a wall.
+const int UncertaintyDistance = 5; // Max uncertainty when finding the distance. ( Used in for sweeping to be sure of the distance ).
+const long CenterAngle = 45; // Angle that the head servo is pointing forward.
+bool Driving = false; // Tests if car is currently driving.
+int foundAngle = -1; // Angle found while sweeping for the furthest distance.
 
 #include <Servo.h>
 
-Servo lServo;
-Servo rServo;
+// Servos/Motors
+Servo lServo; //Left Motor.
+Servo rServo; //Right Motor.
+Servo hServo; //Head Servo.
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(triggerPin, OUTPUT);
-  pinMode(listenPin, INPUT);
-  lServo.attach(rMotorPin);
-  rServo.attach(lMotorPin);
+  pinMode(echoPin, INPUT);
+  lServo.attach(lMotorPin);
+  rServo.attach(rMotorPin);
+  hServo.attach(headPin);
   Serial.begin(9600);
 }
 
-long GetTimeObjectAndBack(){
+/*
+   Find time to the object and back in microseconds.
+*/
+
+long GetTimeObjectAndBack() {
   digitalWrite(triggerPin, LOW);
   delayMicroseconds(4);
 
@@ -33,52 +51,110 @@ long GetTimeObjectAndBack(){
   delayMicroseconds(12);
   digitalWrite(triggerPin, LOW);
 
-  return pulseIn(listenPin, HIGH);
+  return pulseIn(echoPin, HIGH);
 }
 
-int GetDistanceToObject(long TimeTaken){
-  return (340.0*(1.0/1000000.0)*(100.0)*(TimeTaken/2.0));  
+/*
+   Gets the distance from a time in microseconds (using v=d/t and converting into cm).
+*/
+
+int GetDistanceToObject(long TimeTaken) {
+  return (340.0 * (1.0 / 1000000.0) * (100.0) * (TimeTaken / 2.0));
 }
 
-void DriveForward(){
-  lServo.write(90+FLSpeed);
-  rServo.write(90-FRSpeed);
-  delay(10);
+/*
+   Driving function. Drives forwards for ForwardDelay. It then jumps back to the loop to determine the distance. (Also coasts in if it is not driving).
+*/
+
+void Drive() {
+  if (!Driving) {
+    Driving = true;
+    CoastIn();
+  }
+  lServo.write(90 - FLSpeed);
+  rServo.write(90 + FRSpeed);
+  delay(ForwardDelay);
 }
 
-void Turn(){
-  lServo.write(90+TSpeed);
-  rServo.write(90+TSpeed);
-  delay(TDelay);
-  lServo.write(90);
-  rServo.write(90);
-}
+/*
+   CoastIn function. Drives in gradually to prevent slipping. Reaches FLSpeed. (Uses CoastTurnDelay to delay the change).
+*/
 
-void CoastOut(){
-  for(int i=FLSpeed;i>-1;i--){
-    lServo.write(90+i);
-    rServo.write(90-i);
-    delay(10);  
+void CoastIn() {
+  for (int i = 0; i < FLSpeed + 1; i++) {
+    lServo.write(90 - i);
+    rServo.write(90 + i);
+    delay(CoastTurnDelay);
   }
 }
 
-void CoastIn(){
-   for(int i=0;i<FLSpeed+1;i++){
-    lServo.write(90+i);
-    rServo.write(90-i);
-    delay(10);  
-  } 
+/*
+   CoastOut. Drives down gradually to prevent slipping. Reaches speed zero. (Uses CoastTurnDelay to delay the change).
+*/
+
+void CoastOut() {
+  for (int i = FLSpeed; i > -1; i--) {
+    lServo.write(90 - i);
+    rServo.write(90 + i);
+    delay(CoastTurnDelay);
+  }
+}
+
+/*
+   Looks left, right, and forwards to determine the furthest distance. If it is in a corner, that means the longest distance is the best path.
+*/
+
+void Sweep() {
+  int maxDistance = -1;
+  for (int i = 135; i > -46; i -= 90) {
+    if (i < 0)i = 0;
+    hServo.write(i);
+    delay(100);
+    int Distance = GetDistanceToObject(GetTimeObjectAndBack());
+    int Distance2 = 0;
+    bool foundDistance = false;
+
+    do {
+      delay(500);
+      Distance2 = GetDistanceToObject(GetTimeObjectAndBack());
+      if (abs(Distance - Distance2) < UncertaintyDistance) {
+        foundDistance = true;
+      } else {
+        Distance = Distance2;
+      }
+    } while (!foundDistance);
+
+    if (maxDistance < Distance || maxDistance == -1) {
+      foundAngle = i;
+      maxDistance = Distance;
+    }
+    delay(1000);
+  }
+
+  hServo.write(CenterAngle);
+}
+
+/*
+   Turns the car acording to the angle found in the Sweep function.
+*/
+
+void Turn() {
+  if (foundAngle > CenterAngle) {
+    lServo.write(90 + TLSpeed);
+    rServo.write(90 + TLSpeed);
+  } else if (foundAngle < CenterAngle) {
+    lServo.write(90 - TRSpeed);
+    rServo.write(90 - TRSpeed);
+  }
+  delay(TurnDelay);
 }
 
 void loop() {
-  if(GetDistanceToObject(GetTimeObjectAndBack())<MinDistance){
-    Driving = false;
-    CoastOut();
-    Turn();  
-  }else if(!Driving){
-    Driving = true;
-    CoastIn();
-    DriveForward();      
-  }
-  delay(Sleep);
+  Sweep();
+  Turn();
+  do {
+    Drive();
+  } while (GetDistanceToObject(GetTimeObjectAndBack()) > MinDistance);
+  CoastOut();
+  Driving = false;
 }
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# dawsonhack
+# Dawson Hackathon
+[![alt text](https://raw.githubusercontent.com/alexandre-lavoie/arduino-slide/final/Picture/Doge.jpg)]
+
+# Winner
+[![alt text](https://raw.githubusercontent.com/alexandre-lavoie/arduino-slide/final/Picture/Team.jpg)]
+
+Robot winning the creativity and ingenuity award!
+
+# Challenge
+The first Dawson College Hackathon was divided in two sections. The robot had to be able to perform one or both of the following:
+1. Determine the longest distance in a 4x4 meter square.
+2. Creative and ingenous design to the problem.
+
+# Parts
+* Robot Body Kit
+* 2 Wheels with Rubber Grip
+* 1 Swivel Wheel
+* Arduino Nano (CH340)
+* 1 Servo Motor
+* 2 Motors (Driven by 2ch-sn-controller)
+* 1 Feetech 2ch-sn-controller (For Motors)
+* 1 Keychain Powerbank
+
+# Schematic
+
+This is a basic representation of the circuit (using parts found on tinkercad)
+
+[![alt text](https://raw.githubusercontent.com/alexandre-lavoie/arduino-slide/final/Picture/Circuit.png)]
+
+# Coding
+
+All the coding for this robot can be found in the Main package.
+
+# Solutions to Problems Encountered
+
+* The Arduino Nano CH340 requires a special driver that can be found here : http://www.arduined.eu/ch340g-converter-windows-7-driver-download/
+* Cleaning the wheels prevents slipping.
+* The rechargable battery losing power will yield the robot to turn less.
